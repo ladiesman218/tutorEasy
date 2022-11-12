e6a7b24c5a87a323aec002942ca638ef8ae5ed78
ladiesman218 <duncej@gmail.com>
2022-11-12 15:16:08 +0800

    LanguageListVC now doesn't have a banner, collectionView is now a flow layout one

--- TutorEasy/Extensions+Helpers/Global.swift
+++ TutorEasy/Extensions+Helpers/Global.swift
@@ -7,8 +7,8 @@
 
 import UIKit
 
-let serverURL = URL(string: "http://localhost:8080")!     //localhost
-//let serverURL = URL(string: "https://ade4-1-28-216-191.jp.ngrok.io")!     //ngrok
+//let serverURL = URL(string: "http://localhost:8080")!     //localhost
+let serverURL = URL(string: "https://d16f-1-28-216-191.jp.ngrok.io")!     //ngrok
 //let serverURL = URL(string: "http://20.243.114.35:8080")!     //azure
 //let serverURL = URL(string: "http://0.0.0.0:8080")!     // docker production environment
 let baseURL = serverURL.appendingPathComponent("api")
